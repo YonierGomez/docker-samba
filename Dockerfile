ee8c98e68139fb99002eb533aa01dd14e3267a21
Yonier Asprilla <yonieer13@gmail.com>
2024-06-21 01:08:55 -0500

    Fix directory

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,18 @@
 FROM alpine
+
 LABEL maintainer <Yonier Gómez>
+
 RUN apk add --update \
     samba-common-tools \
     samba-client \
     samba-server \
     && rm -rf /var/cache/apk/*
+
 ENV user=neytor \
     password=neytor \
     mygroup=sambita \
     mydir=/download \
     mydirdos=/work
-COPY run.sh /run.sh
-RUN chmod +x /run.sh  # Asegurar que el script tiene permisos de ejecución
-# ENTRYPOINT [ "sh", "-c", "/run.sh" ]
-CMD [ "sh", "/run.sh" ]
+
+COPY run.sh /opt/
+ENTRYPOINT [ "sh", "/opt/run.sh" ] 
--- run.sh
+++ run.sh
@@ -1,66 +1,31 @@
 #!/bin/bash
 
-# Asignar las variables de entorno si están definidas
-user=${user:-neytor}
-password=${password:-neytor}
-mygroup=${mygroup:-sambita}
+# Imprimir todas las variables de entorno
+echo "Environment variables:"
+env
 
-# Mover el smb.conf original a un respaldo
-mv /etc/samba/smb.conf /etc/samba/smb.backup
+mygroup=$mygroup
 
-# Crear un nuevo smb.conf con la configuración global
-cat << EOF > /etc/samba/smb.conf
-[global]
-workgroup = WORKGROUP
-server string = %h server (Samba, Ubuntu)
-security = user
-server role = standalone server
-passdb backend = smbpasswd
-log file = /var/log/samba/log.%m
-max log size = 1000
-protocol = SMB3
-panic action = /usr/share/samba/panic-action %d
-idmap config * : backend = tdb
-hosts allow = 192., 127., ::1, 172.
-hosts deny = 0.0.0.0/0
-#APPLE
-vfs objects = fruit streams_xattr
-fruit:metadata = stream
-fruit:model = MacSamba
-fruit:posix_rename = yes
-fruit:veto_appledouble = no
-fruit:wipe_intentionally_left_blank_rfork = yes
-fruit:delete_empty_adfiles = yes
-fruit:time machine = yes
-EOF
+# Función para crear directorios basados en variables de entorno
+create_directory() {
+    local var_prefix="$1"
+    local dir_path="${!var_prefix}"
+    local dir_name=$(basename "$dir_path")
 
-# Crear el usuario si no existe
-if ! id -u $user &>/dev/null; then
-    echo "=============================================="
-    echo "Creating user $user"
-    echo "=============================================="
-    adduser -D $user
-    echo -e "$password\n$password" | smbpasswd -a -s $user
-    addgroup -g 8888 $mygroup
-    addgroup -S $user $mygroup
-fi
+    if [ -n "$dir_path" ]; then
+        # Create the directory
+        echo ================================================
+        echo Creating directory $dir_path
+        echo ================================================
+        mkdir -p "$dir_path"
+        chgrp -R $mygroup "$dir_path"  # Asignar el grupo recursivamente
+        chmod 770 "$dir_path"
 
-# Crear directorios a partir de las variables de entorno que empiecen con "mydir"
-for var in $(env | grep '^mydir'); do
-    dir_path="${var#*=}"
-    dir_name=$(basename "$dir_path")
-
-    echo "=============================================="
-    echo "Creating directory $dir_path"
-    echo "=============================================="
-    mkdir -p "$dir_path"
-    chgrp -R $mygroup "$dir_path"  # Asignar el grupo recursivamente
-    chmod 770 "$dir_path"
-
-    echo "=============================================="
-    echo "Adding Samba share for $dir_name"
-    echo "=============================================="
-    cat << EOF >> /etc/samba/smb.conf
+        # Add a Samba share configuration
+        echo ================================================
+        echo Adding Samba share for $dir_name
+        echo ================================================
+        cat << EOF >> /etc/samba/smb.conf
 [$dir_name]
 comment = $dir_name
 path = $dir_path
@@ -72,24 +37,36 @@ force group = +$mygroup
 create mask = 0770
 guest ok = no
 EOF
+    fi
+}
+
+# Crear directorios para todas las variables de entorno que empiecen con "mydir"
+for var in $(env | grep '^mydir'); do
+    create_directory "$var"
 done
 
-# Validar la configuración de Samba
-echo "=============================================="
-echo "Validating Samba configuration"
-echo "=============================================="
+# Crear directorios para mydirdos
+create_directory "mydirdos"
+
+# Crear directorios para mydircuatro (si se ha pasado como variable de entorno)
+create_directory "mydircuatro"
+
+# Validar configuración de Samba
+echo ================================================
+echo Validating Samba configuration
+echo ================================================
 testparm -s
 
-# Mostrar las credenciales del usuario
-echo "=============================================="
-echo "These are your credentials"
-echo "=============================================="
-echo "User: $user"
-echo "Password: $password"
+# Mostrar credenciales de usuario
+echo ================================================
+echo Estas son tus credenciales
+echo ================================================
+echo "Usuario: $user"
+echo "Contraseña: $password"
 
 # Iniciar el servidor Samba
-echo "=============================================="
-echo "Access via smb://myIp"
-echo "=============================================="
-echo "Starting the Samba server"
+echo ================================================
+echo Accede a través de smb://myIp
+echo ================================================
+echo Iniciando el servidor Samba
 smbd --foreground --debug-stdout --no-process-group
